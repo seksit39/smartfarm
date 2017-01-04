7315e421025f8f1dfa5564ca3c8d5c9dd801286d
seksit39 <seksit39@outlook.com>
2017-01-04 22:53:17 +0700

    Add files via upload

---
+++ Server/connectAdd.php
@@ -0,0 +1,20 @@
+<?php
+
+	function Connection(){
+		$server="localhost";
+		$user="root";
+		$pass="";
+		$db="smart farm";
+	   	
+		$connection = mysql_connect($server, $user, $pass);
+
+		if (!$connection) {
+	    	die('MySQL ERROR: ' . mysql_error());
+		}
+		
+		mysql_select_db($db) or die( 'MySQL ERROR: '. mysql_error() );
+
+		return $connection;
+	}
+
+?>
